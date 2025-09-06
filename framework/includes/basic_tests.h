c7e67c46c94bbc1d9d1348a3c10904658b9166d9
Matthias Millhoff <mmillhof@c3a6c6.42berlin.de>
2025-09-06 13:09:29 +0200

    basic layout

---
+++ framework/includes/basic_tests.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   basic_tests.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmillhof <mmillhof@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/06 12:10:04 by mmillhof          #+#    #+#             */
+/*   Updated: 2025/09/06 12:35:55 by mmillhof         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BASIC_TESTS_H
+# define BASIC_TESTS_H
+
+# include <string.h>
+
+int	basic_test(void);
+int	null_test(void);
+
+#endif
